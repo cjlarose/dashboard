f1c112c4a77060575656452310eb3ffe8e19131d
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-01-06 15:25:57 -0500

    Prettified hosts/view page

--- application/views/hosts/view.php
+++ application/views/hosts/view.php
@@ -1,16 +1,39 @@
 <?php
+function status_to_label($status) {
+	$classes = array('success', 'warning', 'important', 'notice');
+	$label = array('OK', 'WARNING', 'CRITICAL', 'UNKNOWN');
+	return "<span class=\"label " . $classes[$status]  .  "\">" . $label[$status]  . "</span>";
+}
 
+function boolean_to_label($boolean, $if_true = 'ENABLED', $if_false = 'DISABLED') {
+	if ($boolean == TRUE)
+		return "<span class=\"label success\">{$if_true}</span>";
+	else 
+		return "<span class=\"label\">{$if_false}</span>";
+}
 /*echo "<pre>";
 var_dump($host);
 echo "</pre>";*/
 ?>
 <h2>Host Information</h2>
-<dl>
 <?php
-foreach ($host[$host_address] as $key => $value)
-	echo "<dt>{$key}</dt><dd>{$value}</dd>";
+$host_info = $host[$host_address];
+$table_data = array();
+$table_data[] = array(
+	$host_info['host_id'],
+	$host_info['host_display_name'],
+	$host_info['host_alias'],
+	boolean_to_label($host_info['host_current_state'], 'up', 'down'),
+	$host_info['host_last_check'],
+	$host_info['host_check_output'],
+	boolean_to_label($host_info['host_notifications_enabled']),
+	boolean_to_label($host_info['host_active_checks_enabled']),
+	boolean_to_label($host_info['host_passive_checks_enabled'])
+);
+$this->table->set_heading(array('ID', 'Display Name', 'Alias', 'Current State', 'Last Check', 'Check Output', 'Notifications', 'Active Checks', 'Passive Checks'));
+echo $this->table->generate($table_data);
+$this->table->clear();
 ?>
-</dl>
 
 <h2>Service Information</h2>
 <?php
@@ -19,15 +42,15 @@ foreach ($services[$host_address] as $service_id => $service)
 	$table_data[] = array(
 		//$service['host_object_id'],
 		$service['service_display_name'],
-		$service['service_current_state'],
+		status_to_label($service['service_current_state']),
 		$service['service_next_check'],
 		$service['service_output'],
 		//$service['service_check_command'],
-		$service['service_notifications_enabled'],
-		$service['service_active_checks_enabled'],
-		$service['service_passive_checks_enabled']		
+		boolean_to_label($service['service_notifications_enabled']),
+		boolean_to_label($service['service_active_checks_enabled']),
+		boolean_to_label($service['service_passive_checks_enabled'])		
 	);
-$this->table->set_heading(array('Service', 'Current State', 'Next Check', 'Output'));
+$this->table->set_heading(array('Service', 'Current State', 'Next Check', 'Check Output', 'Notifications', 'Active Checks', 'Passive Checks'));
 echo $this->table->generate($table_data);
 //echo "<pre>";
 //var_dump($services);
