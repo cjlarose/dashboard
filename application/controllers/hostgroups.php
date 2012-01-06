ab5d82fbdfbfed2ab93ea6483ce08b773fd080a5
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-01-06 12:47:11 -0500

    Added bootstrap and Template library

--- application/controllers/hostgroups.php
+++ application/controllers/hostgroups.php
@@ -1,10 +1,16 @@
 <?php
 class Hostgroups extends CI_Controller {
+	
+	public function __construct() {
+		parent::__construct();
+		$this->load->library('Template');
+		$this->load->helper('url');
+	}
 
 	public function index() {
 		$data = array();
-		echo "hello";
-		$this->load->view('hostgroups/index', $data);
+	//	echo "hello";
+		$this->template->render();
 	}	
 
 }
