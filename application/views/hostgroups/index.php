19e42d577764b962366c79cda77fe8e05454b846
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-01-09 18:26:16 -0500

    Added relative times to host and service tables

--- application/views/hostgroups/index.php
+++ application/views/hostgroups/index.php
@@ -8,13 +8,13 @@ foreach ($hostgroups as $hostgroup => $hosts) {
 	$table_data = array();
 	foreach ($hosts as $host_address => $host) {
 		$table_data[] = array(
-			anchor("hosts/view/{$host_address}", $host['host_alias']) . "<br />" . $host_address, 
+			anchor("hosts/view/{$host_address}", $host['host_alias'] . " (" .  $host['host_display_name'] . ")") . "<br />" . $host_address, 
 			boolean_to_label(!$host['host_current_state'], 'up', 'down'),
-			$host['host_last_check'], 
+			relative_time_abbr($host['host_last_check']), 
 			$host['host_check_output']
 		);
 	}	
-	$this->table->set_heading(array('Alias<br />Host Address', 'State', 'Last Check', 'Check Output'));
+	$this->table->set_heading(array('Alias (Display Name)<br />Host Address', 'State', 'Last Check', 'Check Output'));
 	$this->table->set_template(array(
 		'table_open' => '<table border="0" cellpadding="4" cellspacing="0" class="host-table">'
 	));
