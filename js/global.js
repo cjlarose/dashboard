f07b4e999333bee864c7849386d759ec85a809bf
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-01-09 19:05:49 -0500

    Basic ui on hostgroups for expanding rows to put host info

--- js/global.js
+++ js/global.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+	
+	if ($('.host-table').length > 0) {
+		col_span = $('.host-table tr')[0].children.length;
+		console.log(col_span);
+	}
+	
+	$('.host-table tbody tr')
+		.hover(function() {$(this).addClass('hover');}, function() {$(this).removeClass('hover')})
+		.after("<tr class=\"host-info\"><td colspan=\""+col_span+"\">yoyoyo</td></tr>")
+		.click(function() {
+			if (!$(this).hasClass('active')) {
+				// set active class
+				$('.host-table tbody tr.active').removeClass('active').next().hide();
+				$(this).addClass('active');
+				
+				// show host info row
+				$(this).next().show(); 
+			} else {
+				$(this).removeClass('active');
+				$(this).next().hide();	
+			}
+		});
 
-
+	$('tr.host-info').hide();
 });
