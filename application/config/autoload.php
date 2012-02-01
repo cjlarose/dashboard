1b7e51101df86eeb5c38f90e876624a34f32eb0d
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-02-01 15:55:28 -0700

    Added fusion api client to autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('Template', 'NagiosAPI', 'NagiosData');
+$autoload['libraries'] = array('Template', 'NagiosAPI', 'NagiosData', 'FusionAPI');
 
 
 /*
